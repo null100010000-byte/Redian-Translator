cd622e33e8d4d7b6472032066e8665f8ea955a27
null100010000-byte <null100010000@gmail.com>
2026-05-18 12:58:59 +0800

    Update for app.py

--- for app.py
+++ for app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
 CIPHER_MAP = {
-    'a': 'Chinga', 'b': 'Chong', 'c': 'Ching', 'd': 'Cheng',
-    'e': 'Chinge', 'f': 'Fing', 'g': 'Guangdong', 'h': 'Hong',
-    'i': 'Yi', 'j': 'Jiang', 'k': 'Kong', 'l': 'Long',
-    'm': 'Ming', 'n': 'Nigga', 'o': 'Osaka', 'p': 'Penis',
-    'q': 'Qidong', 'r': 'Rice', 's': 'Suck', 't': 'Trachea',
-    'u': 'Underwear', 'v': 'Vietnam', 'w': 'Wongwenxi', 'x': 'Xray',
-    'y': 'Yotta', 'z': 'Zebrawoman', ' ': '-'
+    'a': 'Anal', 'b': 'Butt', 'c': 'Cock', 'd': 'Dick',
+    'e': 'EroMe', 'f': 'Fuck', 'g': 'GayPorn', 'h': 'Hentai',
+    'i': 'InMyAss', 'j': 'JAV', 'k': 'Kut', 'l': 'LickMaBalls',
+    'm': 'Moaning', 'n': 'NiggerSex', 'o': 'Oral', 'p': 'PornHub',
+    'q': 'Quickie', 'r': 'Rape', 's': 'Sex', 't': 'Tits',
+    'u': 'Urethra', 'v': 'Vagina', 'w': 'Wickle', 'x': 'Xvideos',
+    'y': 'Yoon', 'z': '질', ' ': '-'
 }
 REVERSE_MAP = {v.lower(): k for k, v in CIPHER_MAP.items()}
 
@@ -21,20 +21,20 @@ def decode_description(encoded_text):
     return "".join(REVERSE_MAP.get(word.lower(), word) for word in words)
 
 
-st.set_page_config(page_title="Cipher Translator", page_icon="🔐")
+st.set_page_config(page_title="Redian Translator", page_icon="🔐")
 
-st.title("Mingish Translator")
-st.write("Translate Mingish to English or English to Mingish")
+st.title("Redian Translator")
+st.write("Translate Redian to English or English to Redian")
 
-mode = st.sidebar.selectbox("Functions", ["English -> Mingish", "Mingish -> English"])
+mode = st.sidebar.selectbox("Functions", ["English -> Redian", "Redian -> English"])
 
 user_input = st.text_area("Input your text:", placeholder="input your text here")
 
 if st.button("translate"):
     if user_input:
-        if mode == "English -> Mingish":
+        if mode == "English -> Redian":
             result = encrypt_sentence(user_input)
-            st.success("translated to Mingish!")
+            st.success("translated to Redian!")
         else:
             result = decode_description(user_input)
             st.success("translated to English!")
